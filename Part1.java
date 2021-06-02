465e83f813e2fb1d0739e08d8e80b2fb54523891
Ben Shade <bshade0211@myduneland.org>
2021-06-01 17:51:09 -0700

    hi2

--- Part1.java
+++ Part1.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class Part1{
     public static void main(String [] args){
-        Scanner name = new Scanner(System.in);
+        Scanner nick = new Scanner(System.in);
         System.out.println("Welcome Hero to Kings Rising!"); // backround info for story
         System.out.println("You are in the village of Radel, located in the Kingdom of Errasel.");
         System.out.println("The Goblin King has besieged us, you must help us!");
         System.out.println("Travel to the camp and wipe them out!");
         System.out.println("But first, What is your name Hero?");
-        String.input = scan.nextline(); // asks for name
+        String name = nick.nextLine(); // asks for name
         System.out.println("Hello " + name);
     }
 }
